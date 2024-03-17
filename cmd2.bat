2b2f6cd9073a70356b9e1dd2c62678dbad8f2a04
Enry85Moly <molinari.85.enrico@gmail.com>
2024-03-17 10:08:17 +0100

    commit 4

--- cmd2.bat
+++ cmd2.bat
@@ -7,14 +7,14 @@ path=%base%\bin;%path%
 
 del /Q /S .\work\*.o .\work\*.exe >nul
 
-rem Compila tutti i file sorgente .c nella sotto-directory Sw4
-for %%i in (".\Sw4\*.c") do (
-    gcc -pipe -std=c11 -w -ggdb -c -fpermissive -malign-double -O%2 "%%i" -o ".\work\%%~ni.o"
+rem Compila tutti i file sorgente .c nella main root del progetto
+for %%i in ("*.c") do (
+    gcc -pipe -std=c11 -w -ggdb -c -fpermissive -malign-double -O%2 -x c "%%i" -o ".\work\%%~ni.o"
 )
 
 if errorlevel 1 goto end
 
-rem Ottieni tutti i nomi dei file .o nella sotto-directory work
+rem Ottieni tutti i nomi dei file .o nella sotto-directory work del progetto
 set files=
 for %%f in (".\work\*.o") do (
     set files=!files! ".\work\%%~nf.o"
